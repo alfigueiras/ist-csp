2868724e37311d70c639530c57713e2f39d4c19b
António Figueiras <alpfigueiras@gmail.com>
2024-05-23 16:53:16 +0100

    KeyGen changes: appropriate variance

--- keygen.py
+++ keygen.py
@@ -5,12 +5,11 @@ import random as rand
 
 class gswKeys:
     def __init__(self, lbda, L):
-        self.n, self.k, self.q, self.l, self.m, self.N, self.error_dist = self.setup(lbda, L)
+        self.n, self.k, self.q, self.l, self.m, self.N, self.error_distr = self.setup(lbda, L)
         
         self.t,self.sk,self.v=self.secret_key_gen()
 
         self.pk=self.public_key_gen()
-
 
     def setup(self, lbda, L):
         #q with k bits
@@ -22,24 +21,21 @@ class gswKeys:
         l=floor(log2(q))+1
         m=n*l
         N=(n+1)*l
-        error_distr = lambda : floor(np.random.standard_normal())#*q/(8*(N+1)**L))
+        error_distr = lambda size : np.rint(np.random.normal(loc=0, scale=q/8, size=size))
         return n,k,q,l,m,N,error_distr
     
     def public_key_gen(self):
         B=np.random.randint(0, self.q, (self.m, self.n), dtype=np.int64)
-        errors=np.array([self.error_dist() for i in range(self.m)]).T
+        errors=self.error_distr(size=self.m).T
         b=np.matmul(B,self.t)+errors
         b=b.reshape((-1,1))
         pk=np.hstack((b, B))
         return pk
 
     def secret_key_gen(self):
-        t = [-(rand.randint(0,self.q)) for j in range(self.n)]
+        t = [-(rand.randint(0,self.q-1)) for j in range(self.n)]
         sk = [1]+t
         return t,sk,Powersof2(sk, self.l)
     
-a=gswKeys(13,5)
-
-
-
+a=gswKeys(40,10)
 
